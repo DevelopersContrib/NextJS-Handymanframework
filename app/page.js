d53ce9e3e6b0327683331e96ab3e1b0251f11714
jaysoncano <cano.jayson10@gmail.com>
2023-07-19 08:46:56 +0800

    add footer

--- app/page.js
+++ app/page.js
@@ -1,13 +1,79 @@
 import Image from 'next/image'
 import HeaderForm from '@/components/home/HeaderForm'
 import Navigation from '@/components/includes/Navigation';
+import Footer from '@/components/includes/Footer';
 
 export default function Home() {
   const background = 'https://cdn.vnoc.com/background/bg-handyman1.png';
+  const domain ='javapoint.com';
+  const fb_url = 'facebook.com';
+  const twitter_url = 'twitter.com';
+  const linkedin_url = 'linkedin.com';
   return (
     <>
       <Navigation />
       <HeaderForm background={background} />
+      <section className='tw-py-24 tw-bg-white'>
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-12 text-center">
+              <h1 className='tw-text-5xl tw-font-medium tw-mb-4'>Acquired by Handyman.com</h1>
+              <h5 className='tw-mb-4'>
+                Get Matched to a Screened Professional Handyman
+              </h5>
+              <p>Homeremodelinginc.com helps you find an experienced, professional Handyman contractor in your local area, FREE, NO OBLIGATIONS. Homeremodelinginc.com is the industry leading portal for the home improvement, home repair and remodeling industry. Our free tools and services help both homeowners and contractors facilitate the process to accomplish your home repair and remodeling needs. Serving the Home and Contractor market. Why use our pros?</p>
+              <p>For the commercial real estate market or projects over $5,000, Homeremodelinginc.com offers a free consultation service to analyze your needs and find suppliers, general contractors and local professional service providers. Are you lacking a professional website or want to communicate more efficiently with your customer or contractor? Get a Free Webpage and Free Consultation and manage it online. Need professional and experienced contractors, subcontractors or handymen due to an increase in your work load? You have come to the right place at Homeremodelinginc.com, your local referral and local handyman service provider.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='tw-bg-[#E7E7E7]'>
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-3 tw-bg-[#EDD342] tw-border-solid tw-border-0 tw-border-b-[6px]  tw-border-b-white">
+              <a href="#" className='tw-flex tw-w-full tw-flex-col tw-py-8 tw-px-4 tw-text-black tw-no-underline tw-text-center'>
+                <span className="tw-font-semibold tw-uppercase">
+                  post a project
+                </span>
+                <span className="tw-font-semibold tw-capitalize tw-text-xs tw-mt-1">
+                  Home owner post your project free
+                </span>
+              </a>
+            </div>
+            <div className="col-xl-3 hover:tw-bg-[#b1fa62] tw-border-solid tw-border-0 tw-border-b-[6px]  tw-border-b-[#B1FA62] hover:tw-border-b-white">
+              <a href="#" className='tw-flex tw-w-full tw-flex-col tw-py-8 tw-px-4 tw-text-black tw-no-underline tw-text-center'>
+                <span className="tw-font-semibold tw-uppercase">
+                FIND CONTRACTOR
+                </span>
+                <span className="tw-font-semibold tw-capitalize tw-text-xs tw-mt-1">
+                Contractors in Your Area Now  
+                </span>
+              </a>
+            </div>
+            <div className="col-xl-3 hover:tw-bg-[#62e6fa] tw-border-solid tw-border-0 tw-border-b-[6px]  tw-border-b-[#62e6fa] hover:tw-border-b-white">
+              <a href="#" className='tw-flex tw-w-full tw-flex-col tw-py-8 tw-px-4 tw-text-black tw-no-underline tw-text-center'>
+                <span className="tw-font-semibold tw-uppercase">
+                FIND PROJECT
+                </span>
+                <span className="tw-font-semibold tw-capitalize tw-text-xs tw-mt-1">
+                Get Contractor Projects
+                </span>
+              </a>
+            </div>
+            <div className="col-xl-3 hover:tw-bg-[#F6AD89] tw-border-solid tw-border-0 tw-border-b-[6px]  tw-border-b-[#F6AD89] hover:tw-border-b-white">
+              <a href="#" className='tw-flex tw-w-full tw-flex-col tw-py-8 tw-px-4 tw-text-black tw-no-underline tw-text-center'>
+                <span className="tw-font-semibold tw-uppercase">
+                HOW IT WORKS
+                </span>
+                <span className="tw-font-semibold tw-capitalize tw-text-xs tw-mt-1">
+                Free Match-up for your project!   
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer domain={domain} fb_url={fb_url} twitter_url={twitter_url} linkedin_url={linkedin_url} />
     </>
   )
 }
--- components/home/HeaderForm.jsx
+++ components/home/HeaderForm.jsx
@@ -1,29 +1,73 @@
-"use client"
-import Nav from 'react-bootstrap/Nav';
+import { FaFax, FaCircleInfo } from "react-icons/fa6";
 
-const HeaderForm = ({background}) => {
+const HeaderForm = ({ background }) => {
   return (
     <>
       <div className="tw-pt-24 lg:tw-min-h-[600px] tw-bg-cover tw-bg-center tw-relative tw-flex tw-w-full tw-items-center tw-flex-col" style={{ backgroundImage: `url('${background}')` }}>
         <div className="tw-bg-[rgba(17,17,17,0.45)] tw-absolute tw-left-0 tw-top-0 tw-bottom-0 tw-right-0"></div>
         <div className="container text-center tw-relative tw-text-white tw-flex tw-h-full tw-flex-1">
           <div className="row tw-w-full">
-            <div className="col-xl-12">
-              hello
+            <div className="col-xl-8 offset-lg-2 tw-mb-5">
+              {/* Start:: Estimate Form */}
+              <div className="tw-mt-12 ">
+                <h1 className="text-center tw-text-7xl tw-font-bold tw-my-5">
+                  Get an Estimate!
+                </h1>
+                <div className="tw-bg-[rgba(0,0,0,.25)] tw-p-5 tw-rounded-md">
+                  <div className="input-group input-group-lg">
+                    <select className="form-select">
+                      <option selected>Project Type...</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                    <input type="text" className="form-control" placeholder="Zip" />
+                    <button class="btn btn-primary" type="button">Go!</button>
+                  </div>
+                </div>
+              </div>
+              {/* Start:: Expert Form */}
+              <div className="tw-mt-12 tw-hidden">
+                <h1 className="text-center tw-text-7xl tw-font-bold tw-my-5">
+                Ask an Expert
+                </h1>
+                <div className="tw-bg-[rgba(0,0,0,.25)] tw-p-5 tw-rounded-md">
+                  <div className="input-group input-group-lg">
+                    <select className="form-select">
+                      <option selected>Category...</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                    <input type="text" className="form-control" placeholder="Ask" />
+                    <button class="btn btn-primary" type="button">Go!</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div className="container tw-mt-auto tw-relative tw-text-white">
           <div className="row">
             <div className="col-xl-12 text-center">
-              <Nav variant="tabs" activeKey="tab-1">
-                <Nav.Item>
-                  <Nav.Link eventKey="tab-1">Option 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="tab-2">Option 2</Nav.Link>
-                </Nav.Item>
-              </Nav>
+              <ul className="list-inline tw-bg-[#F1F1F1] tw-inline-flex tw-mx-auto tw-px-[1rem_!important] mb-0 tw-rounded-t-md custom-tab-nav">
+                <li className="list-inline-item">
+                  <a href="#" className='active tw-inline-flex tw-uppercase tw-flex-col tw-text-[#464646] tw-font-bold tw-no-underline tw-p-4'>
+                    <span>
+                      <FaFax />
+                    </span>
+                    <span>Get an estimate</span>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#" className='tw-inline-flex tw-uppercase tw-flex-col tw-text-[#464646] tw-font-bold tw-no-underline tw-p-4'>
+                    <span>
+                      <FaCircleInfo />
+                    </span>
+                    <span>ask an expert</span>
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
