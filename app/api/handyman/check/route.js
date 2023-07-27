660c4bfd960c09b9ac0ee94310c6f1379250f077
ron <row.none@gmail.com>
2023-07-27 10:36:50 +0800

    fixed check/route

--- app/api/handyman/check/route.js
+++ app/api/handyman/check/route.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 export const POST = async (request) => {
-  const data = request.json()
-  // console.log('Request:',request)
+  const data = await request.json()
   const url = process.env.HANDYMAN_API + `checkexists`;
-  console.log(data)
+  // console.log(data)
   try {
-    params = new URLSearchParams()
+    const params = new URLSearchParams()
     params.append('field','email')
-    params.append('value','data.email')
-
-    const res = await axios.get(url)
-    console.log(process.env.HANDYMAN_API)
+    params.append('value',data.email)
+    console.log('params',params)
+    const res = await axios.post(url,params)
+    // console.log('url',process.env.HANDYMAN_API)
     console.log(url)
-    console.log(res)
+    // console.log('res',res.data)
 
     return new Response(JSON.stringify(res.data), { status: 201 })
   } catch (error) {
     console.log("Error: ", error);
   }
+
 }
