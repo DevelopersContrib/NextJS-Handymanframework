5aaf4f028faea90b7f03f932ae24fce74c5184c4
julius-ph <71057113+juliusL03@users.noreply.github.com>
2025-03-20 13:53:06 +0800

    added blog and details page

---
+++ app/blog/page.jsx
@@ -0,0 +1,13 @@
+import BlogList from "@/modules/blog/BlogList";
+
+const page = () => {
+  return (
+    <>
+      <div className="tw-min-h-screen tw-pt-5 tw-pb-5 tw-mt-8tw-mb-8">
+        <BlogList />
+      </div>
+    </>
+  );
+};
+
+export default page;
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import HeaderForm from "@/components/home/HeaderForm";
 import TopDomains from "@/components/TopDomains";
+import BlogList from "@/modules/blog/BlogList";
 
 import { getTopsites } from "@/lib/data";
 
@@ -14,34 +15,25 @@ const Home = async () => {
         <div className="container">
           <div className="row">
             <div className="col-xl-12 text-center">
-              <h1 className="tw-text-5xl tw-font-medium tw-mb-4">
-                Acquired by Handyman.com
-              </h1>
-              <h5 className="tw-mb-4">
-                Get Matched to a Screened Professional Handyman
-              </h5>
+              <h1 className="tw-text-5xl tw-font-medium tw-mb-4">Acquired by Handyman.com</h1>
+              <h5 className="tw-mb-4">Get Matched to a Screened Professional Handyman</h5>
               <p>
-                Homeremodelinginc.com helps you find an experienced,
-                professional Handyman contractor in your local area, FREE, NO
-                OBLIGATIONS. Homeremodelinginc.com is the industry leading
-                portal for the home improvement, home repair and remodeling
-                industry. Our free tools and services help both homeowners and
-                contractors facilitate the process to accomplish your home
-                repair and remodeling needs. Serving the Home and Contractor
-                market. Why use our pros?
+                Homeremodelinginc.com helps you find an experienced, professional Handyman
+                contractor in your local area, FREE, NO OBLIGATIONS. Homeremodelinginc.com is the
+                industry leading portal for the home improvement, home repair and remodeling
+                industry. Our free tools and services help both homeowners and contractors
+                facilitate the process to accomplish your home repair and remodeling needs. Serving
+                the Home and Contractor market. Why use our pros?
               </p>
               <p>
-                For the commercial real estate market or projects over $5,000,
-                Homeremodelinginc.com offers a free consultation service to
-                analyze your needs and find suppliers, general contractors and
-                local professional service providers. Are you lacking a
-                professional website or want to communicate more efficiently
-                with your customer or contractor? Get a Free Webpage and Free
-                Consultation and manage it online. Need professional and
-                experienced contractors, subcontractors or handymen due to an
-                increase in your work load? You have come to the right place at
-                Homeremodelinginc.com, your local referral and local handyman
-                service provider.
+                For the commercial real estate market or projects over $5,000, Homeremodelinginc.com
+                offers a free consultation service to analyze your needs and find suppliers, general
+                contractors and local professional service providers. Are you lacking a professional
+                website or want to communicate more efficiently with your customer or contractor?
+                Get a Free Webpage and Free Consultation and manage it online. Need professional and
+                experienced contractors, subcontractors or handymen due to an increase in your work
+                load? You have come to the right place at Homeremodelinginc.com, your local referral
+                and local handyman service provider.
               </p>
             </div>
           </div>
@@ -55,9 +47,7 @@ const Home = async () => {
                 href="/project/post"
                 className="tw-flex tw-w-full tw-flex-col tw-py-8 tw-px-4 tw-text-black tw-no-underline tw-text-center"
               >
-                <span className="tw-font-semibold tw-uppercase">
-                  post a project
-                </span>
+                <span className="tw-font-semibold tw-uppercase">post a project</span>
                 <span className="tw-font-semibold tw-capitalize tw-text-xs tw-mt-1">
                   Home owner post your project free
                 </span>
@@ -68,9 +58,7 @@ const Home = async () => {
                 href="https://handyman.com/contractor/find"
                 className="tw-flex tw-w-full tw-flex-col tw-py-8 tw-px-4 tw-text-black tw-no-underline tw-text-center"
               >
-                <span className="tw-font-semibold tw-uppercase">
-                  FIND CONTRACTOR
-                </span>
+                <span className="tw-font-semibold tw-uppercase">FIND CONTRACTOR</span>
                 <span className="tw-font-semibold tw-capitalize tw-text-xs tw-mt-1">
                   Contractors in Your Area Now
                 </span>
@@ -81,9 +69,7 @@ const Home = async () => {
                 href="https://handyman.com/project/find"
                 className="tw-flex tw-w-full tw-flex-col tw-py-8 tw-px-4 tw-text-black tw-no-underline tw-text-center"
               >
-                <span className="tw-font-semibold tw-uppercase">
-                  FIND PROJECT
-                </span>
+                <span className="tw-font-semibold tw-uppercase">FIND PROJECT</span>
                 <span className="tw-font-semibold tw-capitalize tw-text-xs tw-mt-1">
                   Get Contractor Projects
                 </span>
@@ -94,9 +80,7 @@ const Home = async () => {
                 href="https://handyman.com/home/how-it-works"
                 className="tw-flex tw-w-full tw-flex-col tw-py-8 tw-px-4 tw-text-black tw-no-underline tw-text-center"
               >
-                <span className="tw-font-semibold tw-uppercase">
-                  HOW IT WORKS
-                </span>
+                <span className="tw-font-semibold tw-uppercase">HOW IT WORKS</span>
                 <span className="tw-font-semibold tw-capitalize tw-text-xs tw-mt-1">
                   Free Match-up for your project!
                 </span>
@@ -112,6 +96,9 @@ const Home = async () => {
               <TopDomains domains={domains} />
             </div>
           </div>
+        </div>
+        <div className="container">
+          <BlogList />
         </div>
       </section>
     </>
